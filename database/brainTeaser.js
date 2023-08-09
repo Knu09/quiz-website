7c3fcac1bfa58468ceecda3e98a81404d578d66e
Christian L. De La Torre <132640979+Knu09@users.noreply.github.com>
2023-08-10 00:35:29 +0800

    Add files via upload

---
+++ database/brainTeaser.js
@@ -0,0 +1,167 @@
+const questions_BT = [
+    {
+        num: 1,
+        question: "Who is considered the \"father of modern computers\"? ",
+        answer: "Alan Turing"
+    },
+    {
+        num: 2,
+        question: "What was the first computer virus?  ",
+        answer: "The Creeper Program",
+    },
+    {
+        num: 3,
+        question: "Who is considered the \"father of computing\"" +
+            " and is known for designing the Analytical Engine? ",
+        answer: "Charles babbage",
+    },
+    {
+        num: 4,
+        question: "What computer term is attributed to John W. Tukey and refers to the process of processing and analyzing large datasets?  ",
+        answer: "",
+    },
+    {
+        num: 5,
+        question: "What was the first personal computer created by IBM, released in 1981?  ",
+        answer: "IBM 5150 Personal Computer"
+    },
+    {
+        num: 6,
+        question: "Who is credited with creating the World Wide Web (WWW) and developing the first web browser? ",
+        answer: "Tim Berners-Lee"
+    },
+    {
+        num: 7,
+        question: "Which company released the first smartphone, known as \"Simon Personal Communicator,\" in 1994? ",
+        answer: "BMI"
+    },
+    {
+        num: 8,
+        question: "What is the official mascot of College of Information Technology? ",
+        answer: "Falcon"
+    },
+    {
+        num: 9,
+        question: "Who is our current dean in our college? ",
+        answer: "Elmer Haro"
+    },
+    {
+        num: 10,
+        question: "Who is the Saint of this building? ",
+        answer: "Ezekiel Moreno"
+    },
+    {
+        num: 11,
+        question: "Complete the acronym RICHNEST. ",
+        answer: ""
+    },
+    {
+        num: 12,
+        question: "What does CPU stand for? ",
+        answer: "Central Processing Unit"
+    },
+    {
+        num: 13,
+        question: "What is the primary function of the CPU? ",
+        answer: ""
+    },
+    {
+        num: 14,
+        question: "Name two types of computer memory. ",
+        answer: "Random Access Memory and Read-Only Memory"
+    },
+    {
+        num: 15,
+        question: "What is the purpose of RAM in a computer? ",
+        answer: ""
+    },
+    {
+        num: 16,
+        question: "What does the acronym \"WWW\" stand for? ",
+        answer: "World Wide Web"
+    },
+    {
+        num: 17,
+        question: "What is a web browser? ",
+        answer: ""
+    },
+    {
+        num: 18,
+        question: "What is an operating system? ",
+        answer: ""
+    },
+    {
+        num: 19,
+        question: "Name three popular operating systems for personal computers. ",
+        answer: "Microsoft Windows MacOS Linux"
+    },
+    {
+        num: 20,
+        question: "What does HTML stand for? ",
+        answer: "HyperText Markup Language"
+    },
+    {
+        num: 21,
+        question: "What is the purpose of HTML in web development?  ",
+        answer: ""
+    },
+    {
+        num: 22,
+        question: "What does CSS stand for?  ",
+        answer: "Cascading Style Sheets"
+    },
+    {
+        num: 23,
+        question: "What is the role of CSS in web development? ",
+        answer: ""
+    },
+    {
+        num: 24,
+        question: "What is a URL?  ",
+        answer: "Uniform Source Locators"
+    },
+    {
+        num: 25,
+        question: "What is a virus?  ",
+        answer: ""
+    },
+    {
+        num: 26,
+        question: "What is a firewall?  ",
+        answer: ""
+    },
+    {
+        num: 27,
+        question: "What is the purpose of a computer mouse? ",
+        answer: ""
+    },
+    {
+        num: 28,
+        question: `<p>JavaScript:<br>
+            let a = 2;<br>
+            const b = 3;<br>
+            b = 4;<br>
+            console.log(a + b);</p>`,
+        answer: "Error"
+    },
+    {
+        num: 29,
+        question: `
+            <p>JavaScript:<br>
+            let a = 2;<br>
+            const b = 3;<br>
+            console.log("a" + "b");</p>`,
+        answer: "ab"
+    },
+    {
+        num: 30,
+        question:`
+            <p>JavaScript:<br>
+            let a = "2";<br>
+            const b = "3";<br>
+            console.log(a+b);</p>
+        `,
+        answer: "23"
+    }
+
+]
---
+++ database/machineProblem.js
@@ -0,0 +1,20 @@
+let questions = [
+    {
+        num: 1,
+        question: "Print “Hello World” in 5 different programming languages. ",
+    },
+    {
+        num: 2,
+        question: "Create a program with a sorting algorithm (any sorting will do), however use pen and paper only. ",
+    },
+    {
+        num: 3,
+        question: "Write a program that requires a user integer input to display a half pyramid and full pyramid using" +
+            " asterisk or any other symbol by using the Fibonacci sequence."
+    },
+    {
+        num: 4,
+        question: "Write a program that checks a string if it is a palindrome and if it is an anagram. "
+    },
+
+]
